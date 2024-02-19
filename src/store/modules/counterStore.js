17f04c86d8ffb46e37f00ef465c55037a8e6009f
Lv Jingxin <lv510987@163.com>
2024-02-19 09:12:40 +0800

    feat: store模块化方法存储

--- src/store/modules/counterStore.js
+++ src/store/modules/counterStore.js
@@ -2,9 +2,10 @@
  * @Author: Lv Jingxin lv510987@163.com
  * @Date: 2024-02-01 18:15:23
  * @LastEditors: Lv Jingxin lv510987@163.com
- * @LastEditTime: 2024-02-02 09:53:04
+ * @LastEditTime: 2024-02-19 09:12:11
  * @FilePath: /react-redux/src/store/modules/counterStore.js
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @Description: 相应方法存入store模块化
+ * 
  */
 import { createSlice } from "@reduxjs/toolkit";
 const counterStore = createSlice({
