9490382510af75cfd7b85851666df4daf990f3a8
Lv Jingxin <lv510987@163.com>
2024-03-19 09:49:06 +0800

    feat: 测试

--- src/App.js
+++ src/App.js
@@ -2,35 +2,37 @@
  * @Author: Lv Jingxin lv510987@163.com
  * @Date: 2024-02-01 16:43:52
  * @LastEditors: Lv Jingxin lv510987@163.com
- * @LastEditTime: 2024-02-19 09:48:01
+ * @LastEditTime: 2024-03-19 09:48:21
  * @FilePath: /react-redux/src/App.js
  * @Description: store中存储方法
  */
 import { useDispatch, useSelector } from "react-redux";
-import {inscrement,decrement,addToNum}from "./store/modules/counterStore"
+import { inscrement, decrement, addToNum } from "./store/modules/counterStore";
 import { featchannlList } from "./store/modules/channelStore";
 import { useEffect } from "react";
 function App() {
-  const {count } = useSelector(state=>state.counter)
-  const {channelList} = useSelector(state => state.channel)
-  const dispatch = useDispatch()
+  const { count } = useSelector((state) => state.counter);
+  const { channelList } = useSelector((state) => state.channel);
+  const dispatch = useDispatch();
   // 使用useEffect触发异步请求
-  useEffect(()=>{
-    dispatch(featchannlList())
-  },[dispatch])
+  useEffect(() => {
+    dispatch(featchannlList());
+  }, [dispatch]);
   return (
     <div className="App">
-      <button onClick={()=>dispatch(decrement())}>--</button>
-    {count}
-      <button onClick={()=>dispatch(inscrement())}>++</button>
-    <button onClick={()=>dispatch(addToNum(10))}>add to 10</button>
-    <button onClick={()=>dispatch(addToNum(20))}>add to 20</button>
+      <button onClick={() => dispatch(decrement())}>--</button>
+      {count}
+      <button onClick={() => dispatch(inscrement())}>++</button>
+      <button onClick={() => dispatch(addToNum(10))}>add to 10</button>
+      <button onClick={() => dispatch(addToNum(20))}>add to 20</button>
 
-    <ul>
-      {channelList.map(item=> <li key={item.id}>{item.name}</li>)}
-    </ul>
+      <ul>
+        {channelList.map((item) => (
+          <li key={item.id}>{item.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
-
+//
 export default App;
