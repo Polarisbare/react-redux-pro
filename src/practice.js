c128d8d54f3ae0622b98ca583bcf75de6834a604
Lv Jingxin <lv510987@163.com>
2024-03-27 09:14:51 +0800

    feat: memo and

---
+++ src/practice.js
@@ -0,0 +1,5 @@
+// import { memo } from "react";
+
+// memo   用于缓存，props  参数改变时重新渲染，否则不渲染 memo（）
+// 他的机制是，在使用memo缓存组件之后，react会对每个props进行Object.is比较新值和老值，true没变化，false有变化
+// 当props的类型是简单类型用memo（）复杂类型用useMemo(()=>{return []},[]ß)
